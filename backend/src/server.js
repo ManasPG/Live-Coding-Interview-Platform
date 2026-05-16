a2a25b5534635f2d0d6f5cfef9942df61fa84818
ManasPG <manasgoswamee@gmail.com>
2026-05-16 14:45:14 +0530

    CLerk Auth and MongoDB Integrated

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connnectDb } from "./lib/db.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Server Running" });
+  res.status(200).json({ msg: "Bitch Please" });
 });
 
-app.listen(ENV.PORT, () => console.log(`Server running on port ${ENV.PORT}`));
+const startServer = async () => {
+  try {
+    await connnectDb();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on port: ", ENV.PORT),
+    );
+  } catch (error) {
+    console.error("💥Error Starting the server:", error);
+  }
+};
+
+startServer();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  console.warn(
+    "Missing Clerk publishable key (VITE_CLERK_PUBLISHABLE_KEY). Auth may not work without it.",
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
