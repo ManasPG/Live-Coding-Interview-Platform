61d6654b048806db5f6c4564f8991e0d86500800
ManasPG <manasgoswamee@gmail.com>
2026-05-18 02:22:58 +0530

    Edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import { ENV } from "./lib/env.js";
 import { connnectDb } from "./lib/db.js";
 
 import { serve } from "inngest/express";
-import { inngest } from "./lib/inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 //credentails:true meaning?? server allows cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-app.use("/api/inngest", serve);
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bitch Please" });
